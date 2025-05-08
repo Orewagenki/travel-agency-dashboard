9e4e00bd0d060a4522907d75258183c718d368ef
Orewagenki <orewagenki3@gmail.com>
2025-05-08 07:47:18 -0600

    Update NavItems.tsx

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,6 +1,13 @@
+import {LINK} from 'react-router';
+
 const NavItems = () => {
   return (
-    <div>NavItems</div>
+      <section className="nav-items">
+        <Link to='/' className="link-logo">
+          <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]"/>
+          <h1>Tourvisto</h1>
+        </Link>
+        </section>
     )
 }
 
