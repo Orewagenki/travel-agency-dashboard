c9e861e48853658046c1c4efdb329124a2d904c7
Orewagenki <orewagenki3@gmail.com>
2025-05-09 10:52:59 -0600

    Update Header.tsx
    added dynamic styles depending on what dashboard the user is seeing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,25 @@
-const Header = () => {
+import {useLocation} from "react-router";
+
+interface Props{
+  title: string;
+  descrpition: string;
+}
+
+
+const Header = ({title,description}: Props) => {
+const location = useLocation();
+  
   return(
-      <div>Header</div>
+      <header className="header">
+        <article>
+          <h1 className={cn("text-dark-100",
+                location.pathname === '/' ? 'text-2xl md:text-4xl font-bold' :
+                'text-xl md:text-2xl font-semibold')}>{title}</h1>
+          <p className={cn("text-grey-100" "font-normal",
+                location.pathname === '/' ? 'text-base md:text-lg font-bold' :
+                'text-sm md:text-lg')}>{description}</p>
+        </article>
+      </header>
     )
 }
 
